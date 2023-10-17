65afb8f29e6a7a519275e23c4ea55b91b281a7b3
Abdón Hernández <102083933+abdoonhdz@users.noreply.github.com>
2023-10-17 16:08:54 +0200

    Add files via upload

---
+++ window/script.js
@@ -0,0 +1,17 @@
+let mainElement = document.querySelector('main');
+
+function crearElementos() {
+    let copo = document.createElement("div");
+    copo.textContent = "❄";
+    copo.classList.add('copo');
+    copo.style.left = `${Math.random() * 99}%`;
+    copo.style.top = '0';
+    document.querySelector('main').appendChild(copo);
+
+    setTimeout(function() {
+        mainElement.removeChild(copo);
+    }, 3500);
+}
+
+let intervalo = 650;
+setInterval(crearElementos, intervalo);
