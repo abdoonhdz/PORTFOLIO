2403a5e4b880d8158d9e188cffe723e2500bca18
Abdón Hernández <abdonhc70@gmail.com>
2023-12-14 10:38:27 +0100

    comentarios

--- proyectos/window/script.js
+++ proyectos/window/script.js
@@ -1,21 +1,33 @@
+// Obtener una referencia al elemento principal del documento (main)
 let mainElement = document.querySelector('main');
 
+// Función para crear elementos (copos de nieve) y agregarlos al elemento principal
 function crearElementos() {
+    // Crear un elemento div para representar un copo de nieve
     let copo = document.createElement("div");
+
+    // Asignar el carácter de copo de nieve y añadir la clase 'copo' para estilos
     copo.textContent = "❄";
     copo.classList.add('copo');
+
+    // Asignar posiciones aleatorias dentro del elemento principal
     copo.style.left = `${Math.random() * 99}%`;
     copo.style.top = '0';
+
+    // Agregar el copo al elemento principal del DOM (main)
     document.querySelector('main').appendChild(copo);
 
-    setTimeout(function() {
+    // Establecer un temporizador para eliminar el copo después de 3500 milisegundos (3.5 segundos)
+    setTimeout(function () {
         mainElement.removeChild(copo);
     }, 3500);
 
-    copo.addEventListener('click', function() {
+    // Agregar un evento de clic para eliminar el copo cuando se hace clic en él
+    copo.addEventListener('click', function () {
         mainElement.removeChild(copo);
     });
 }
 
+// Establecer un intervalo para llamar a la función crearElementos cada 650 milisegundos (0.65 segundos)
 let intervalo = 650;
 setInterval(crearElementos, intervalo);
